91d4bfcd48963585214d07ebbc936019d71dfbcf
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2024-01-02 21:34:29 -0300

    descripción e instrucciones del calendario

---
+++ Calendario_Emocional/src/components/Layout.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+export default class Layout extends Component {
+  static propTypes = {second: third}
+
+  render() {
+    return (
+      <div>
+        <div>
+            <p>Conocer lo que sentimos, darle nombre y expresarlo es el principio de la salud emocional y llevar un calendario (o diario) puede ser un instrumento de gran ayuda.</p>
+            <p>A la hora de expresar cómo nos hemos sentido durante el día, este tipo de registros nos permite ser conscientes de esas emociones, qué situaciones las han disparado, cómo nos hemos comportado, cómo hemos anticipado que irían a mejor o peor y, además, nos permite reflexionar y encontrar formas para gestionar mejor esos sentimientos. En otras palabras, nos ayuda a desarrollar nuestra inteligencia emocional y, consecuentemente, nos traen un mayor bienestar. </p>
+        </div>
+        <div>
+            <p>Antes de iniciar tu calendario es conveniente que tengas una percepción de lo que, para ti, ha sido este año que termina. Así que comienza haciendo dos listas paralelas (dos columnas), en una de ellas anota los retos a los que te enfrentaste en el año, en la otra escribe las emociones que has sentido en cada una de esas situaciones. </p>
+            <p>Este primer paso te permitirá identificar aquellos aspectos en los que debes trabajar el próximo año. Así te prepararás para nuevos retos posibles en los siguientes meses. </p>
+        </div>
+        <div>
+            <p>
+            Para plantear objetivos emocionales hay que ser realistas, así evitaremos prontas frustraciones. Escribe objetivos pequeños y alcanzables, fáciles de cumplir y que te aporten plena satisfacción. 
+            </p>
+            <p>
+            La clave es dosificarlos y sentir constantemente las recompensas del buen resultado. En este caso, el calendario de salud emocional puede ayudarte a tener registro de cualquier tipo de meta y emociones implicadas.  
+            </p>
+            <p>
+            Por ejemplo, si tu meta es meditar al menos 3 veces a la semana, como un comienzo, reporta las emociones que se involucran en tu práctica: cómo te sientes al practicar, qué pasa cuando no practicas, qué dificultades tienes, etc.  
+            </p>
+        </div>
+        <div>
+            <p>En realidad, este tipo calendarios o diarios tiene como esencia que se registren en un papel o notebook toda clase de emociones tal y como se han sentido. Basta con una libreta y, de forma sistemática, empezar a expresarnos para gestionar lo que sentimos.  </p>
+            <p>Se puede hacer la anotación cada día o en aquellos momentos donde las emociones son muy intensas. Lo ideal sería apuntar cada día, poniendo tanto las emociones consideradas positivas, como aquellas que nos hacen sentir incómodos.  </p>
+            <p>Pero si quieres algo mucho más gráfico y ornamental, que lo tengas siempre a la vista o lo lleves en tu agenda, puedes diseñar un calendario de la siguiente manera: </p>
+            <ul>
+                <li>En un costado de la página, dibuja pequeños cuadros o círculos y en cada uno destina un color a cada emoción.  </li>
+                <li>Haz una cuadrícula en la que anotes los meses del año y los días a cada mes (un calendario en forma) </li>
+                <li>Cada día que pase colorea el cuadro correspondiente según la emoción que predominó esa jornada.  </li>
+            </ul>
+        </div>
+      </div>
+    )
+  }
+}
