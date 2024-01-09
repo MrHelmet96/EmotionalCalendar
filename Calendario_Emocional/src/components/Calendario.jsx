9d25760ef4ad16b459d102f40a4e7e69aedef1bc
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2024-01-08 22:10:24 -0300

    style changes

--- Calendario_Emocional/src/components/Calendario.jsx
+++ Calendario_Emocional/src/components/Calendario.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
-import '../assets/styles/styles.css'; // Asegúrate de crear este archivo CSS
+import '../assets/styles/styles.css';
 
 function Calendario() {
   const dias = Array.from({ length: 31 }, (_, i) => i + 1);
   const meses = ['E', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'];
   
-  // Función para obtener la cantidad de días en un mes
-  const getDiasEnMes = (mes, año) => {
-    return new Date(año, mes, 0).getDate();
-  };
-
   return (
     <div className="calendario">
-      <div className="fila">
-        <div className="celda"></div> {/* Celda vacía en la esquina superior izquierda */}
+      <div className="fila fila-meses">
+        
         {meses.map((mes, i) => (
-          <div key={i} className="celda">{mes}</div>
+          <div key={i} className="mes">{mes}</div>
         ))}
       </div>
       {dias.map((dia, i) => (
         <div key={i} className="fila">
-          <div className="celda">{dia}</div>
+          <div className="dia">{dia}</div>
           {meses.map((mes, j) => (
-            <div key={j} className="celda"></div> /* Celdas vacías para los días */
+            <div key={j} className="celda"></div>
           ))}
         </div>
       ))}
